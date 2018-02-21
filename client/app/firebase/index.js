a3f5d0060a51a17df470c4953e101aa22ea30eec
Matt Pace <matthew.dp.96@gmail.com>
2018-02-21 12:37:59 -0500

    Add firebase to mobile app

---
+++ client/app/firebase/index.js
@@ -0,0 +1,26 @@
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+
+import { FIREBASE_API_KEY, FIREBASE_SENDER_ID } from 'react-native-dotenv';
+
+const config = {
+  apiKey: FIREBASE_API_KEY,
+  authDomain: "meetoverdb.firebaseapp.com",
+  databaseURL: "https://meetoverdb.firebaseio.com",
+  projectId: "meetoverdb",
+  storageBucket: "meetoverdb.appspot.com",
+  messagingSenderId: FIREBASE_SENDER_ID
+};
+
+const app = firebase.initializeApp(config);
+
+export const fetchIdToken = (token) =>
+  firebase.auth().signInWithCustomToken(token)
+    .then(() => {
+      return firebase.auth().currentUser.getIdToken(true);
+    })
+    .catch(err => {
+      console.log(`Could not initialize Firebase: ${err}`);
+
+      throw err;
+    });

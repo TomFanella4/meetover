cb87176a3c3e82864220de1ea8edd1a1211902e7
Tom Fanella <fanella@purdue.edu>
2018-03-03 17:44:31 -0500

    Added image asset for current location

--- client/app/App.js
+++ client/app/App.js
@@ -53,10 +53,9 @@ export default class App extends React.Component {
 
   _loadResourcesAsync = async () => {
     return Promise.all([
-      // Asset.loadAsync([
-      //   require('./assets/images/robot-dev.png'),
-      //   require('./assets/images/robot-prod.png'),
-      // ]),
+      Asset.loadAsync([
+        require('../assets/images/current_location.png'),
+      ]),
       Font.loadAsync({
         // This is the font that we are using for our tab bar
         ...Ionicons.font,

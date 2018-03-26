4d0adcb47b3d9cd30e630de897f6560f2927ea1a
Tom Fanella <fanella@purdue.edu>
2018-03-26 19:25:56 -0400

    Implement Screens for chat

--- client/app/screens/ChatScreen.js
+++ client/app/screens/ChatScreen.js
@@ -1,91 +1,114 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
-import { GiftedChat } from 'react-native-gifted-chat'
-import * as firebase from 'firebase';
+import { connect } from 'react-redux';
+import {
+  Spinner,
+  View,
+  Button
+} from 'native-base';
+import { GiftedChat, Bubble } from 'react-native-gifted-chat';
 
-import { PTSansText } from '../components/StyledText'
+import { PTSansText } from '../components/StyledText';
+import Colors from '../constants/Colors';
+import { sendFirebaseMessage } from '../firebase';
+import {
+  registerFetchNewMessageAsync,
+  fetchEarlierMessagesAsync
+} from '../actions/chatActions';
 
-export default class ChatScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Chat',
-  };
+const messagesToLoad = 50;
 
-  state = {
-    messages: [],
-  }
-
-  componentWillMount() {
-    // this.setState({
-    //   messages: [
-    //     {
-    //       _id: 1,
-    //       text: 'Hello developer',
-    //       createdAt: new Date(),
-    //       user: {
-    //         _id: 2,
-    //         name: 'React Native',
-    //         avatar: 'https://i.imgur.com/wq43v5T.jpg',
-    //       },
-    //     },
-    //   ],
-    // })
-    // console.log(this.state.messages);
-
-    var userId = firebase.auth().currentUser.uid;
-    var messages = firebase.database().ref('messages/' + userId);
-    messages.on('value', snapshot => {
-      console.log(snapshot.val());
-      snapshot.val() &&
-      this.setState(previousState => ({
-        messages: snapshot.val().messages
-      }))
-    })
-    // .then(snapshot => {
-    //   console.log(snapshot.val());
-    // })
-    // .catch(err => console.error(err));
-  }
+class ChatScreen extends React.Component {
+  static navigationOptions = ({ navigation }) => ({
+    title: `Chat with ${navigation.state.params.name}`,
+  });
 
   onSend(messages = []) {
-    // this.setState(previousState => ({
-    //   messages: GiftedChat.append(previousState.messages, messages),
-    // }))
-    var userId = firebase.auth().currentUser.uid;
-    firebase.database().ref('messages/' + userId).set({
-      messages: GiftedChat.append(this.state.messages, messages),
-      // messages: [
-      //     {
-      //       _id: 1,
-      //       text: 'Hello developer',
-      //       createdAt: JSON.stringify(new Date()),
-      //       user: {
-      //         _id: 2,
-      //         name: 'React Native',
-      //         avatar: 'https://i.imgur.com/wq43v5T.jpg',
-      //       },
-      //     },
-      //   ],
-    });
+    const _id = this.props.navigation.state.params._id;
+
+    sendFirebaseMessage(_id, messages);
+  }
+
+  onLoadEarlier() {
+    const {
+      messages,
+      navigation,
+      fetchEarlierMessagesAsync,
+    } = this.props;
+    const _id = navigation.state.params._id;
+
+    fetchEarlierMessagesAsync(_id, messagesToLoad, messages[messages.length - 1]._id);
+  }
+
+  renderBubble (props) {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          right: {
+            backgroundColor: Colors.tintColor
+          }
+        }}
+      />
+    )
   }
 
   render() {
-    // console.log(this.state.messages);
+    const { navigation, userProfile, messages } = this.props;
     return (
-      <GiftedChat
-        messages={this.state.messages}
-        onSend={messages => this.onSend(messages)}
-        user={{
-          _id: 1,
-        }}
-      />
+      <View style={styles.container}>
+        <GiftedChat
+          messages={messages}
+          onSend={messages => this.onSend(messages)}
+          messageIdGenerator={() => null}
+          user={{
+            _id: userProfile.id,
+            name: userProfile.formattedName,
+            avatar: userProfile.pictureUrl
+          }}
+          loadEarlier={messages && messages[messages.length - 1]._id !== 0}
+          onLoadEarlier={() => this.onLoadEarlier()}
+          renderLoading={() => <Spinner color={Colors.tintColor} />}
+          renderBubble={this.renderBubble}
+          inverted={true}
+        />
+      </View>
     );
   }
+
+  componentDidMount() {
+    const {
+      messages,
+      navigation,
+      registerFetchNewMessageAsync,
+      fetchEarlierMessagesAsync
+    } = this.props;
+
+    if (!messages) {
+      fetchEarlierMessagesAsync(navigation.state.params._id, messagesToLoad);
+      registerFetchNewMessageAsync(navigation.state.params._id);
+    }
+  }
 };
+
+const mapStateToProps = (state, ownProps) => ({
+  userProfile: state.userProfile,
+  messages: state.chat.messageThreads[ownProps.navigation.state.params._id]
+});
+
+const mapDispatchToProps = {
+  registerFetchNewMessageAsync,
+  fetchEarlierMessagesAsync
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ChatScreen);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 15,
     backgroundColor: '#fff',
   }
 });

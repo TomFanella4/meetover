70be29b260e84f0fe8b5bf5ec858f1cc54604ce7
Tom Fanella <fanella@purdue.edu>
2018-03-26 19:25:56 -0400

    Implement Screens for chat

--- client/app/screens/ChatsScreen.js
+++ client/app/screens/ChatsScreen.js
@@ -1,38 +1,90 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
-import { Container, Content, List, ListItem } from 'native-base';
+import { Container, Content, View, List, ListItem, Spinner } from 'native-base';
+import { connect } from 'react-redux';
 
-import { PTSansText } from '../components/StyledText'
+import { PTSansText } from '../components/StyledText';
+import Colors from '../constants/Colors';
+import { chatScreenStrings } from '../constants/Strings';
 import IsSearchingBar from '../components/IsSearchingBar';
+import {
+  registerFetchThreadListAsync,
+} from '../actions/chatActions';
 
-export default class ChatsScreen extends React.Component {
+class ChatsScreen extends React.Component {
   static navigationOptions = {
     title: 'Chats',
   };
 
-  _viewChatThread() {
-    this.props.navigation.navigate('ChatScreen');
+  _viewChatThread(_id, name) {
+    this.props.navigation.navigate('ChatScreen', {
+      _id, name
+    });
   }
 
   render() {
+    const { threadList } = this.props;
+
+    const threadItems = threadList ?
+      threadList.map(thread => (
+        <ListItem
+          key={thread.name}
+          onPress={() => this._viewChatThread(thread._id, thread.name)}
+        >
+          <PTSansText>{thread.name}</PTSansText>
+        </ListItem>
+      ))
+    :
+      null;
+
     return (
       <Container style={styles.container}>
         <IsSearchingBar />
-        <Content>
-          <List>
-            <ListItem onPress={() => this._viewChatThread()}>
-              <PTSansText>Krutarth Rao</PTSansText>
-            </ListItem>
-          </List>
-        </Content>
+        {
+          threadItems ?
+            threadItems.length > 0 ?
+              <Content>
+                <List>
+                  {threadItems}
+                </List>
+              </Content>
+            :
+              <View style={styles.noChatsText}>
+                <PTSansText>{chatScreenStrings.noChatsText}</PTSansText>
+              </View>
+          :
+            <Spinner color={Colors.tintColor} />
+        }
       </Container>
     );
   }
+
+  componentDidMount() {
+    this.props.registerFetchThreadListAsync();
+  }
 };
+
+const mapStateToProps = state => ({
+  threadList: state.chat.threadList
+});
+
+const mapDispatchToProps = {
+  registerFetchThreadListAsync
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ChatsScreen);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+  },
+  noChatsText: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
   }
 });

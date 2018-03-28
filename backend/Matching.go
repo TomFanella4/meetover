46d83c67e160e81efd26b3edca1269f39e9f2a94
raokrutarth <krk91@outlook.com>
2018-03-27 23:36:49 -0400

    ml model generator and file cleaner imported

--- backend/Matching.go
+++ backend/Matching.go
@@ -1,11 +1,21 @@
 package main
 
+import "errors"
+
 //TODO:
 // - take user ID and find k cachedUsers that are:
 //		- in the same location
 //		- will be closest in similarity metric that is picked
 
 // GetMatches returns k user profiles that the UserID will want to meet
+
 func GetMatches(UserID string, neighbors []User) ([]User, error) {
+	callingUser := GetUser(userID)
+	emptyUSer := User{}
+	if emptyUSer == emptyUSer {
+		return []User{}, errors.New("Unable to fetch calling user")
+	}
+
+
 	return cachedUsers, nil
 }

4c5e00c9bce91e0af674a18efecf5ff5aa44c4e5
Matt Pace <matthew.dp.96@gmail.com>
2018-03-29 23:11:40 -0400

    Add seedUser

--- backend/Handlers.go
+++ backend/Handlers.go
@@ -65,6 +65,24 @@ func Test(w http.ResponseWriter, r *http.Request) {
 	tt := params["testType"]
 	if tt == "profile" {
 		json.NewEncoder(w).Encode(strings.Replace(sampleProfile, "\n", "", -1))
+	} else if tt == "seedUser" {
+		users, err := fbClient.Ref("/users")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		umap := make(map[string]User, len(cachedUsers))
+
+		for _, u := range cachedUsers {
+			u.Profile.FormattedName = u.Profile.FirstName + " " + u.Profile.LastName
+			u.Profile.ID = u.ID
+			umap[u.ID] = u
+		}
+
+		if err := users.Update(umap); err != nil {
+			fmt.Println(err)
+		}
 	}
 }
 

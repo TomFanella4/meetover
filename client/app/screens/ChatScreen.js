4603c0f103042efdbd6e93e618d6bf5e94a4752a
Tom Fanella <fanella@purdue.edu>
2018-04-16 16:31:28 -0400

    Add review modal

--- client/app/screens/ChatScreen.js
+++ client/app/screens/ChatScreen.js
@@ -4,15 +4,17 @@ import { connect } from 'react-redux';
 import {
   Spinner,
   View,
-  Button
+  Button,
+  Text
 } from 'native-base';
 import { GiftedChat, Bubble } from 'react-native-gifted-chat';
 
+import MeetOverModal from '../components/MeetOverModal';
 import { PTSansText } from '../components/StyledText';
 import { ProfileImage } from '../components/ProfileImage';
 import { chatMessagesToLoad } from '../constants/Common';
 import Colors from '../constants/Colors';
-import { sendFirebaseMessage } from '../firebase';
+import { sendFirebaseMessage, modifyFirebaseUserState } from '../firebase';
 import {
   registerFetchNewMessageAsync,
   fetchEarlierMessagesAsync
@@ -20,26 +22,31 @@ import {
 
 class ChatScreen extends React.Component {
   static navigationOptions = ({ navigation }) => ({
-    title: `${navigation.state.params.profile.formattedName}`,
+    title: navigation.state.params.profile.formattedName,
     headerRight: (
       <Button
-        style={styles.headerButton}
-        onPress={() => navigation.navigate('ProfileScreen', { profile: navigation.state.params.profile })}
+        onPress={navigation.state.params._handleConclude}
         transparent
+        style={styles.headerButton}
       >
-        <ProfileImage style={styles.headerThumbnail} pictureUrl={navigation.state.params.profile.pictureUrl} />
+        <Text style={styles.headerText}>Conclude</Text>
       </Button>
     )
   });
 
-  onSend(messages = []) {
+  state = {
+    isModalVisible: false,
+    modalViewType: !this.props.navigation.state.params.review ? 'conclude' : 'completed'
+  }
+
+  _handleSend(messages = []) {
     const _id = this.props.navigation.state.params._id;
     const access_token = this.props.userProfile.token.access_token;
 
     sendFirebaseMessage(_id, messages, access_token);
   }
 
-  onLoadEarlier() {
+  _handleLoadEarlier() {
     const {
       messages,
       navigation,
@@ -50,7 +57,12 @@ class ChatScreen extends React.Component {
     fetchEarlierMessagesAsync(_id, chatMessagesToLoad, messages[messages.length - 1]._id);
   }
 
-  renderBubble (props) {
+  _handlePressAvatar() {
+    const { navigation } = this.props;
+    navigation.navigate('ProfileScreen', { profile: navigation.state.params.profile });
+  }
+
+  renderBubble(props) {
     return (
       <Bubble
         {...props}
@@ -63,13 +75,68 @@ class ChatScreen extends React.Component {
     )
   }
 
+  async _handleSubmitReview(review) {
+    const threadID = this.props.navigation.state.params._id;
+    await modifyFirebaseUserState(`threadList/${threadID}/review`, review);
+    this.setState({ modalViewType: 'completed' });
+  }
+
+  _renderConcludeModal() {
+    return (
+      <View>
+        <PTSansText style={styles.modalHeaderText}>How was your MeetOver experience?</PTSansText>
+        <Button style={styles.modalButton} onPress={() => this._handleSubmitReview('positive')}>
+          <PTSansText style={styles.modalText}>It was a good match</PTSansText>
+        </Button>
+        <Button style={styles.modalButton} onPress={() => this._handleSubmitReview('negative')}>
+          <PTSansText style={styles.modalText}>It wasn't a good match</PTSansText>
+        </Button>
+        <Button style={styles.modalButton} onPress={() => this._handleSubmitReview('none')}>
+          <PTSansText style={styles.modalText}>We didn't meet</PTSansText>
+        </Button>
+      </View>
+    );
+  }
+
+  _renderCompletedModal() {
+    return (
+      <View>
+        <PTSansText style={styles.modalHeaderText}>Thanks for your feedback!</PTSansText>
+      </View>
+    );
+  }
+
   render() {
     const { navigation, userProfile, messages } = this.props;
+    const { isModalVisible, modalViewType } = this.state;
+
+    let modal;
+    switch(modalViewType) {
+      case 'conclude':
+        modal = this._renderConcludeModal();
+        break;
+
+      case 'completed':
+        modal = this._renderCompletedModal();
+        if (isModalVisible) {
+          this.modalTimeout = setTimeout(() => this.setState({ isModalVisible: false }), 2000);
+        } else {
+          clearTimeout(this.modalTimeout);
+        }
+        break;
+    }
+
     return (
       <View style={styles.container}>
+        <MeetOverModal
+          isVisible={isModalVisible}
+          onBackdropPress={() => this.setState({ isModalVisible: false })}
+        >
+          {modal}
+        </MeetOverModal>
         <GiftedChat
           messages={messages}
-          onSend={messages => this.onSend(messages)}
+          onSend={messages => this._handleSend(messages)}
           messageIdGenerator={() => null}
           user={{
             _id: userProfile.id,
@@ -77,7 +144,8 @@ class ChatScreen extends React.Component {
             avatar: userProfile.pictureUrl
           }}
           loadEarlier={messages && messages[messages.length - 1]._id !== 0}
-          onLoadEarlier={() => this.onLoadEarlier()}
+          onLoadEarlier={() => this._handleLoadEarlier()}
+          onPressAvatar={() => this._handlePressAvatar()}
           renderLoading={() => <Spinner color={Colors.tintColor} />}
           renderBubble={this.renderBubble}
           inverted={true}
@@ -93,6 +161,8 @@ class ChatScreen extends React.Component {
       registerFetchNewMessageAsync,
       fetchEarlierMessagesAsync
     } = this.props;
+
+    navigation.setParams({ _handleConclude: () => this.setState({ isModalVisible: true }) });
 
     if (!messages) {
       fetchEarlierMessagesAsync(navigation.state.params._id, chatMessagesToLoad);
@@ -122,11 +192,20 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
   },
   headerButton: {
-    padding: 20,
     alignSelf: 'center'
   },
-  headerThumbnail: {
-    width: 35,
-    height: 35
+  headerText: {
+    color: '#fff'
+  },
+  modalButton: {
+    marginTop: 20,
+    backgroundColor: Colors.tintColor,
+    alignSelf: 'center'
+  },
+  modalHeaderText: {
+    alignSelf: 'center'
+  },
+  modalText: {
+    fontSize: 18
   }
 });
